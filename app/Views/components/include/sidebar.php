7f98b2de80bb27c252b9b23a03bfd8c0e2626153
chandra-halimm <chan.halim98@gmail.com>
2025-08-07 23:39:13 +0700

    ahhh males dan lemes

--- app/Views/components/include/sidebar.php
+++ app/Views/components/include/sidebar.php
@@ -3,8 +3,7 @@
 				<h3 class="weight-600 font-16 text-blue">
 					Layout Settings
 					<span class="btn-block font-weight-400 font-12"
-						>User Interface Settings</span
-					>
+						>User Interface Settings</span>
 				</h3>
 				<div class="close-sidebar" data-toggle="right-sidebar-close">
 					<i class="icon-copy ion-close-round"></i>
